23f13f80263e6e7076ed3f598f22bec4805a8f1c
yaroslav.n <yaroslav.sib@yandex.ru>
2015-02-12 15:17:49 +0600

    second task

---
+++ Home_2.php
@@ -0,0 +1,30 @@
+<?php 
+
+$brands = array(
+    array(
+      'name' => 'Oracle',
+      'popularity' => 5
+    ),
+    array(
+      'name' => 'Microsoft',
+      'popularity' => 10
+    ),
+    array(
+      'name' => '1C',
+      'popularity' => '2'
+    )
+);
+
+foreach ($brands as $key => $row) {
+    $name[$key]  = $row['name'];
+    $popularity[$key] = $row['popularity'];
+}
+ 
+array_multisort($popularity, SORT_DESC, $name, SORT_ASC, $brands);
+
+
+//testing
+echo $brands[0]['popularity']."\n".$brands[0]['name']."\n";
+echo $brands[1]['popularity']."\n".$brands[1]['name']."\n";
+echo $brands[2]['popularity']."\n".$brands[2]['name']."\n";
+?>
