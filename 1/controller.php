303d090123c3c3b490c7b5f0bf410587e2b910e3
yaroslav.n <yaroslav.sib@yandex.ru>
2015-02-15 13:21:09 +0600

    mvc

---
+++ 1/controller.php
@@ -0,0 +1,6 @@
+<?php
+
+require_once('model.php');
+require_once('view.php');
+
+print_array($array);
---
+++ 1/view.php
@@ -0,0 +1,9 @@
+<?php
+
+function print_array($array){
+	$i=1;
+	foreach($array as $value){
+		echo "$i. $value\n<br>";
+		$i++;
+	}
+}
